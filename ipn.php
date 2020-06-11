e3c4709d3146eb43d3ac8483602fbb0e572fdd9e
Lucas <lr@creacttive.com>
2020-06-10 21:53:05 -0300

    change debug

--- ipn.php
+++ ipn.php
@@ -1,7 +1,5 @@
 <?php
 $content = file_get_contents('php://input');
-if(!empty($content)){
-    $file = fopen('./logs/' . date('Y-m-d') . '.txt', 'a');
-    fwrite($file, $content);
-    fclose($file);
-}
+$file = fopen('ipn.txt', 'a');
+fwrite($file, $content);
+fclose($file);
