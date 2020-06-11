c810dfa4d0fc34c838ff70bd4b1639b3e787a50f
Lucas <lr@creacttive.com>
2020-06-10 22:33:14 -0300

    change log

--- ipn.php
+++ ipn.php
@@ -1,9 +1,14 @@
 <?php
+function logger($content){
+    $file = fopen('ipn.txt', 'a');
+    fwrite($file, $content);
+    fclose($file);
+}
+
 if(isset($_GET) && is_array($_GET) && count($_GET) > 0){
     $content = print_r($_GET, true);
-}else{
-    $content = file_get_contents('php://input');
+    logger($content);
 }
-$file = fopen('ipn.txt', 'a');
-fwrite($file, $content);
-fclose($file);
+
+$content = file_get_contents('php://input');
+logger($content);
